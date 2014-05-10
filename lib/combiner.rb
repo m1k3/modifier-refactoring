cd60f63aa9783af41b18b0fbbff90737bf586ea5
Michal Olah <olahmichal@gmail.com>
2014-05-10 10:08:22 +0200

    first pass at refactoring the combiner
    just moved around the code into private methods. Haven't done anything major to the way the code actually works.

--- lib/combiner.rb
+++ lib/combiner.rb
@@ -10,48 +10,63 @@ class Combiner
     @key_extractor = key_extractor
   end
 
-  def key(value)
-    value.nil? ? nil : @key_extractor.call(value)
-  end
-
   def combine(*enumerators)
     Enumerator.new do |yielder|
       last_values = Array.new(enumerators.size)
       done = enumerators.all? { |enumerator| enumerator.nil? }
       while not done
-        last_values.each_with_index do |value, index|
-          if value.nil? and not enumerators[index].nil?
-            begin
-              last_values[index] = enumerators[index].next
-            rescue StopIteration
-              enumerators[index] = nil
-            end
-          end
-        end
+        pick_candidate_values_from_enums(last_values, enumerators)
 
         done = enumerators.all? { |enumerator| enumerator.nil? } and last_values.compact.empty?
         unless done
-          min_key = last_values.map { |e| key(e) }.min do |a, b|
-            if a.nil? and b.nil?
-              0
-            elsif a.nil?
-              1
-            elsif b.nil?
-              -1
-            else
-              a <=> b
-            end
-          end
-          values = Array.new(last_values.size)
-          last_values.each_with_index do |value, index|
-            if key(value) == min_key
-              values[index] = value
-              last_values[index] = nil
-            end
-          end
+          values = pick_values_from_candidate_values(last_values)
+
           yielder.yield(values)
         end
       end
     end
   end
+
+  private
+    def key(value)
+      value.nil? ? nil : @key_extractor.call(value)
+    end
+
+    def pick_values_from_candidate_values(last_values)
+      min_key = min_key(last_values)
+      values = Array.new(last_values.size)
+      last_values.each_with_index do |value, index|
+        if key(value) == min_key
+          values[index] = value
+          last_values[index] = nil
+        end
+      end
+      values
+    end
+
+    def pick_candidate_values_from_enums(last_values, enumerators)
+      last_values.each_with_index do |value, index|
+        if value.nil? and not enumerators[index].nil?
+          begin
+            last_values[index] = enumerators[index].next
+          rescue StopIteration
+            enumerators[index] = nil
+          end
+        end
+      end
+    end
+
+    def min_key(last_values)
+      last_values.map { |e| key(e) }.min do |a, b|
+        if a.nil? and b.nil?
+          0
+        elsif a.nil?
+          1
+        elsif b.nil?
+          -1
+        else
+          a <=> b
+        end
+      end
+    end
 end
